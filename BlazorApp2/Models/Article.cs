8c3a248849a95128e4fa2df29308001340e41138
HyunSeongKil <aha1492@gmail.com>
2020-10-07 20:38:50 +0900

    add Models

---
+++ BlazorApp2/Models/Article.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Models
+{
+    public class Article
+    {
+
+        /// <summary>
+        /// 제목
+        /// </summary>
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Sj { get; set; }
+
+        /// <summary>
+        /// 내용
+        /// </summary>
+        [System.ComponentModel.DataAnnotations.Required]        
+        public string Cn { get; set; }
+        
+        /// <summary>
+        /// 태그
+        /// </summary>
+        public string Tag { get; set; }
+        
+        /// <summary>
+        /// 카테고리 코드
+        /// </summary>
+        public string CategoryCode { get; set; }
+
+    }
+}
---
+++ BlazorApp2/Models/DetailCode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Models
+{
+    /// <summary>
+    /// 상세 코드
+    /// </summary>
+    public class DetailCode
+    {
+        /// <summary>
+        /// 상세 코드 아이디
+        /// </summary>
+        public string DetailCodeId { get; set; }
+
+        /// <summary>
+        /// 상세 코드 명
+        /// </summary>
+        public string DetailCodeNm { get; set; }
+
+        /// <summary>
+        /// 그룹 코드 아이디
+        /// </summary>
+        public string GroupCodeId { get; set; }
+    }
+}
