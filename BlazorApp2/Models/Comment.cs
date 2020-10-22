7fe0b5285b485126bcb516c2b4da81174c71796f
HyunSeongKil <aha1492@gmail.com>
2020-10-22 20:17:54 +0900

    fix

---
+++ BlazorApp2/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Models
+{
+    public class Comment
+    {
+        public string CommentId { get; set; }
+        public string CubeId { get; set; }
+        public string UserId { get; set; }
+        public string UserNm { get; set; }
+        public string Cn { get; set; }
+        public DateTime RegistDt { get; set; }
+    }
+}
